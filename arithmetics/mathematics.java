d8a86dd9a956a496fdfc8bf50a26200b36b62d9b
Hey Bro <zufarjonxojiakbarov@gmail.com>
2026-01-21 16:35:23 +0500

    asd
    asdasd

--- arithmetics/mathematics.java
+++ arithmetics/mathematics.java
@@ -6,3 +6,12 @@ public class mathematics {
         System.out.println(result);
     }
 }
+
+//public class mathematics {
+//    public static void main(String [] args){
+//        int result = 8 + 16 / 4 * (6 - 4) - 3 + 12 / (2 * 3) * 5 - (7 - 5) * 2;
+//        //reuslt is 19
+//
+//        System.out.println(result);
+//    }
+//}
